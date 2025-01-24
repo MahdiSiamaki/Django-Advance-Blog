110ce347d61d8c812b27339dfc49e2edf3735abe
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-24 15:07:04 +0330

    Add APIView classes in api V1

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('posts/', views.post_list, name='post-list'),
-    path('posts/<int:pk>/', views.post_detail, name='post-detail'),
+    # path('posts/', views.post_list, name='post-list'),
+    # path('posts/<int:pk>/', views.post_detail, name='post-detail'),
+    path('posts/', views.PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import PostSerializer
 from blog.models import Post
 from rest_framework import status
 
+"""
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def post_list(request):
@@ -20,8 +23,29 @@ def post_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
 
-@api_view(['GET','PUT','DELETE'])
+class PostList(APIView):
+    """List all posts or create a new post"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self,request):
+        """List all posts"""
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        """Create a new post"""
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+'''@api_view(['GET','PUT','DELETE'])
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'GET':
@@ -35,4 +59,30 @@ def post_detail(request,pk):
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'detail':'Post deleted'},status=status.HTTP_204_NO_CONTENT)'''
+
+class PostDetail(APIView):
+    """Retrieve, update or delete a post instance"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self,request,pk):
+        """Retrieve a post"""
+        post = get_object_or_404(Post,pk=pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    def put(self,request,pk):
+        """Update a post"""
+        post = get_object_or_404(Post,pk=pk)
+        serializer = self.serializer_class(post,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk):
+        """Delete a post"""
+        post = get_object_or_404(Post,pk=pk)
+        post.delete()
+        return Response({'detail':'Post deleted'},status=status.HTTP_204_NO_CONTENT)
