2547c57d06364c53e30eb5fea13bb0f19ef1ec1e
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-28 17:31:04 +0330

    Split profile and account urls to better scalability

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,19 +1,14 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
 # from rest_framework.authtoken.views import ObtainAuthToken
-from .views import CustomTokenObtainPairView, ChangePasswordView
+from .. import views
+from ..views import CustomTokenObtainPairView, ChangePasswordView, CustomObtainAuthToken
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
 
-
-
-from . import views
-from .views import CustomObtainAuthToken
-
-app_name = 'api-v1'
 
 urlpatterns = [
     path('register/', views.RegisterApiView.as_view(), name='register'),
@@ -23,5 +18,4 @@ urlpatterns = [
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
