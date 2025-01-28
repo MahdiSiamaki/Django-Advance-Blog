cdbb3f655f1075c4add1c4f88a1474532ba24983
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-28 14:29:24 +0330

    Add simple-jwt endpoints

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
 # from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+
 
 from . import views
 from .views import CustomObtainAuthToken
@@ -11,4 +18,7 @@ urlpatterns = [
     path('register/', views.RegisterApiView.as_view(), name='register'),
     path('token/login/', CustomObtainAuthToken.as_view(), name='token-login'),
     path('token/logout/', views.Logout.as_view(), name='token-logout'),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt_create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
 ]
