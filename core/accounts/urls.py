8912318af54bf1aca3d32e8ce2b9e22ba8ee7501
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-30 20:43:07 +0330

    Add djoser as API v2

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,5 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),  # This includes login, logout, etc
     path('register/', RegisterView.as_view(), name='register'),
-    path('api/v1/', include('accounts.api.v1.urls')),
+    # path('api/v1/', include('accounts.api.v1.urls')),
+    path('api/v2/', include('djoser.urls')),
+    path('api/v2/', include('djoser.urls.jwt')),
 ]
